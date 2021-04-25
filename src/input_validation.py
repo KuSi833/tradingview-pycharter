411642969a7896cfc7d4e5b63cc1d4a13d1d869a
Karlo <e14912km@student.manchester.ac.uk>
2021-04-25 19:40:08 +0200

    Color validation

--- src/input_validation.py
+++ src/input_validation.py
@@ -1,9 +1,18 @@
 import re
 from exceptions import InvalidNameException
+from tv_variables import colors
 
-def validate_chart_name(chart_name: str) -> bool:
+def validate_chart_name(chart_name: str) -> None:
     regex = "^[a-zA-Z_][a-zA-Z0-9_]*$"
     pattern = re.compile(regex)
     result = pattern.search(chart_name)
     if result is None:
         raise InvalidNameException(f"Invalid chart name, should be of form: {regex}")
+
+def validate_colour(color: str) -> None:
+    if color not in colors:
+        regex = "^#[a-zA-Z0-9]{6}$"
+        pattern = re.compile(regex)
+        result = pattern.search(color)
+        if result is None:
+            raise InvalidNameException("Invalid color")
---
+++ src/tv_variables.py
@@ -0,0 +1,6 @@
+colors = {
+    "color.black", "color.blue", "color.fuchsia", "color.gray",
+    "color.green", "color.lime", "color.maroon", "color.navy",
+    "color.olive", "color.orange", "color.purple", "color.red",
+    "color.silver", "color.teal", "color.white", "color.yellow"
+}
