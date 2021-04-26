7c0e99fea0eed55ff6e45ea1514664eba29dc739
Karlo <e14912km@student.manchester.ac.uk>
2021-04-26 22:59:39 +0200

    Charter improvements

--- src/Charter.py
+++ src/Charter.py
@@ -2,21 +2,32 @@ import input_validation
 from element import Element
 from typing import List
 
+
 class Charter():
     def __init__(self, chart_name: str, timeframe: int) -> None:
         self.set_chart_name(chart_name)
         self.timeframe = timeframe
 
         # Initialising element field
-        self.objects = List[Element]
+        self.elements: List[Element] = []
 
-        # self.instructions = []
-        # self.instructions.append(f"//@version=4")
-        # self.instructions.append(f"study('{chart_name}', overlay=true)")
+        self.pine_instruction = (
+            f"//@version=4\n"
+            f"study('{chart_name}', overlay=true)"
+        )
 
     def set_chart_name(self, chart_name: str) -> None:
         input_validation.validate_name(chart_name)
         self.chart_name = chart_name
 
     def add_element(self, element: Element) -> None:
-        self.objects.append("test")
+        self.elements.append(element)
+
+    def __str__(self) -> str:
+        return self.pine_instruction
+
+    def output_pinescript(self) -> None:
+        with open("instruction.pine", "w") as file:
+            file.write(str(self) + "\n")
+            for element in self.elements:
+                file.write(str(element) + "\n")
