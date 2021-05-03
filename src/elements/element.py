5a0bb04fc318ebd6c409840861abf3b2297697e7
Karlo <e14912km@student.manchester.ac.uk>
2021-05-03 15:44:53 +0200

    Abstart class element -> update

---
+++ src/elements/element.py
@@ -0,0 +1,18 @@
+from input_validation import validate_name
+from abc import ABC, abstractmethod
+
+class Element(ABC):
+    def __init__(self, id: str = None) -> None:
+        "Base class of all elements on chart"
+        if id is None:
+            self.id = None
+        else:
+            self.set_id(id)
+
+    @abstractmethod
+    def to_pinescript(self):
+        pass
+
+    def set_id(self, id):
+        validate_name(id)
+        self.id = id
