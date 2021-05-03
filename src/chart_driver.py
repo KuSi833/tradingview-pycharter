c0fcfc12219da2890a5c980a152de311ed88e351
Karlo <e14912km@student.manchester.ac.uk>
2021-05-03 15:45:32 +0200

    Testing

--- src/chart_driver.py
+++ src/chart_driver.py
@@ -1,9 +1,11 @@
 from Charter import Charter
-from label import Label
+from elements.label import Label
+from elements.line import Line
 from point import Point
-from tv_variables import Color, Xloc
+from tv_variables import Extend, Xloc, Color
 
-myCharter = Charter("chartex2", 1000*60*60)
-myLabel = Label(Point(2000, 1619168400000), "test text", xloc=Xloc.BAR_TIME, color=Color.MAROON, initiate=True, id="label1")
-myCharter.add_element(myLabel)
+myCharter = Charter("chartex2", 1000*60*60*24)
+myLabel = Label(Point(1619168400000, 2000), "LINE IS HERE", xloc=Xloc.BAR_TIME, color=Color.MAROON, id="label1")
+myLine = Line(Point(1619168400000, 2000), Point(1619268400000, 2500), extend=Extend.BOTH, color=Color.LIME)
+myCharter.add_elements(myLabel, myLine)
 myCharter.output_pinescript()
