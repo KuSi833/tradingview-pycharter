a9ba9d39ac575e97ab07b3dead3f630cf8394743
Karlo <e14912km@student.manchester.ac.uk>
2021-05-04 16:22:02 +0200

    Plot class

--- src/elements/Plot.py
+++ src/elements/Plot.py
@@ -1,55 +1,68 @@
 from elements.Element import Element
 from PricePoint import PricePoint
-from tv_variables import Color, PlotStyle, Series, Xloc, Extend, LineStyle
+from tv_variables import Color, Display, PlotStyle, Series, Xloc, Extend, LineStyle
 from helpers.formatting import parameter_formatting
-from typing import Union
+from typing import Union, Any
 
 
 class Plot(Element):
-    def __init__(self, series: Union[Series, int, any],
+    def __init__(self, series: Union[Series, int, Any],
                  title: str = None,
                  color: Color = None,
                  linewidth: int = None,
                  style: PlotStyle = None,
-
+                 trackprice: bool = None,
+                 histbase: float = None,
+                 offset: int = None,
+                 join: bool = None,
+                 editable: bool = None,
+                 show_last: int = None,
+                 display: Display = None,
                  id: str = None
                  ) -> None:
         """
         Series can be any build in pinescript series, or a constant
         Any is also an otpion for more complex plotting, but no support for that is provided
         """
-
         super().__init__(id)
         self.series = series
-        self.xloc = xloc
-        self.extend = extend
+        self.title = title
         self.color = color
+        self.linewidth = linewidth
         self.style = style
-        self.width = width
+        self.trackprice = trackprice
+        self.histbase = histbase
+        self.offset = offset
+        self.join = join
+        self.editable = editable
+        self.show_last = show_last
+        self.display = display
 
     def to_pinescript(self):
         self.pine_instruction: str = ""
-
-        # Make sure script runs only once
-        self.pine_instruction += "if barstate.islast\n    "
-
         # Initialisation
         if self.id is not None:
             self.pine_instruction += f"{self.id} = "
 
+        # Pinescript function
+        self.pine_instruction += "plot("
+
         # Required parameters
-        self.pine_instruction += (
-            f"line.new(x1={self.p1.timestamp}, y1={self.p1.price}, "
-            f"x2={self.p2.timestamp}, y2={self.p2.price}, "
-            f"xloc={self.xloc.value}"
-        )
+        self.pine_instruction += parameter_formatting(self.series, "series", add_comma=False)
 
         # Optional parameters
         parameters = [
-            (self.extend, "extend"),
+            (self.title, "title"),
             (self.color, "color"),
+            (self.linewidth, "linewidth"),
             (self.style, "style"),
-            (self.width, "width")
+            (self.trackprice, "trackprice"),
+            (self.histbase, "histbase"),
+            (self.offset, "offset"),
+            (self.join, "join"),
+            (self.editable, "editable"),
+            (self.show_last, "show_last"),
+            (self.display, "display")
         ]
         for parameter, ps_parameter_name in parameters:
             self.pine_instruction += parameter_formatting(
