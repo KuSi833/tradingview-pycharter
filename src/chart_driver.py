ace2113e62cc289f065f7fef51057bf502eea906
Karlo <e14912km@student.manchester.ac.uk>
2021-05-04 18:19:46 +0200

    Testing

--- src/chart_driver.py
+++ src/chart_driver.py
@@ -5,19 +5,15 @@ from elements.Line import Line
 from elements.Fill import Fill
 from PricePoint import PricePoint
 from elements.Plot import Plot
+from elements.compound.Square import Square
 from tv_variables import Extend, LineStyle, Xloc, Color, HlineStyle
 
 myCharter = Charter("chartex2", 1000 * 60 * 60 * 24)
+
 myLabel = Label(PricePoint(1619168400000, 2000))
-# hline1 = Hline(3500, color=Color.BLUE,
-#                linestyle=HlineStyle.DASHED, id="hline1")
-# hline2 = Hline(3000, color=Color.BLUE,
-#                linestyle=HlineStyle.DASHED, id="hline2")
-# fill = Fill(hline1=hline1, hline2=hline2, color=Color.BLUE,
-#             time_start=1618957756000, time_end=1619908156000)
-# line1 = Line(PricePoint(timestamp=1618957756000, price=3000),
-#              PricePoint(timestamp=1619908156000, price=3500))
-# myCharter.add_elements(myLabel, hline1, hline2, fill, line1)
-myPlot = Plot(5000)
-myCharter.add_elements(myLabel, myPlot)
+square = Square(PricePoint(timestamp=1618957756000, price=4000),
+                PricePoint(timestamp=1619908156000, price=5000),
+                color=Color.OLIVE)
+
+myCharter.add_elements(myLabel, square)
 myCharter.output_pinescript()
--- src/elements/compound/Measure.py
+++ src/elements/compound/Measure.py
@@ -1,23 +1,33 @@
-from elements.Element import Element
-from PricePoint import PricePoint
-from tv_variables import Color
-from elements.Label import Label
-from elements.Fill import Fill
+# from elements.Element import Element
+# from PricePoint import PricePoint
+# from tv_variables import Color
+# from elements.Fill import Fill
+# from elements.Label import Label
+# from elements.Plot import Plot
 
 
-class Measure(Element):
-    def __init__(self, p1: PricePoint, p2: PricePoint) -> None:
+# class Measure(Element):
+#     def __init__(self, p1: PricePoint, p2: PricePoint) -> None:
 
-        super().__init__(id)
-        self.p1 = p1
-        self.p2 = p2
+#         super().__init__(id)
+#         self.p1 = p1
+#         self.p2 = p2
 
-        # Constants (can be modified)
-        self.profit_color: Color = Color.GREEN
-        self.loss_color: Color = Color.RED
+#         # Constants (can be modified)
+#         self.profit_color: Color = Color.GREEN
+#         self.loss_color: Color = Color.RED
 
-        # Testing for Measure Color
-        if (p2.price >= p1.price):  # Profit
-            self.color = self.profit_color
-        else:
-            self.color = self.loss_color
+#         # Testing for Measure Color
+#         if (p2.price >= p1.price):  # Profit
+#             self.color = self.profit_color
+#         else:
+#             self.color = self.loss_color
+
+#         # Base Element Scaffolding
+
+#         # Hlines
+#         self.hline1 = Plot(self.p1.price, color=Color.NA)
+#         self.hline2 = Plot(self.p2.price, color=Color.NA)
+
+#         # Background coloring
+#         self.fill = Fill(hl)
