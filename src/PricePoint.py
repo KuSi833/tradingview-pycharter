146e89b9b71cdeea44d9e59d4f99b0f47deaa7e1
Karlo <e14912km@student.manchester.ac.uk>
2021-05-05 00:48:20 +0200

    PricePoint price: int -> float

--- src/PricePoint.py
+++ src/PricePoint.py
@@ -1,4 +1,4 @@
 class PricePoint():
-    def __init__(self, timestamp: int, price: int) -> None:
+    def __init__(self, timestamp: int, price: float) -> None:
         self.timestamp = timestamp
         self.price = price
