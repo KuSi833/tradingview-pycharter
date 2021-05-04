e2ebfdf5951cbeff2800321050cd6b23d7b39755
Karlo <e14912km@student.manchester.ac.uk>
2021-05-05 00:49:21 +0200

    Snap to timeframe and timestamp to string formatting

--- src/helpers/formatting.py
+++ src/helpers/formatting.py
@@ -1,5 +1,8 @@
+from datetime import time
 from enum import Enum
 from typing import Union
+
+from constants.constants import Timeframe
 
 
 def parameter_formatting(parameter: Union[bool, int, str, Enum], ps_parameter_name: str, add_comma: bool = True) -> str:
@@ -22,5 +25,30 @@ def parameter_formatting(parameter: Union[bool, int, str, Enum], ps_parameter_na
         returnstring += str(parameter)
     elif isinstance(parameter, Enum):  # Enum formatting
         returnstring += parameter.value
+
+    return returnstring
+
+
+def snap_to_timeframe(timestamp: int, timeframe: Union[Timeframe, int]) -> int:
+    if isinstance(timeframe, Timeframe):
+        return (timestamp // timeframe.value) * timeframe.value
+    else:
+        return (timestamp // timeframe) * timeframe
+
+
+def timestamp_to_string(timestamp: int) -> str:
+    days = timestamp // Timeframe.D1.value
+    timestamp -= days * Timeframe.D1.value
+    hours = timestamp // Timeframe.H1.value
+    timestamp -= hours * Timeframe.H1.value
+    minutes = timestamp // Timeframe.M1.value
+
+    returnstring = ""
+    if days > 0:
+        returnstring += f"{days}d "
+    if hours > 0:
+        returnstring += f"{hours}h "
+    if minutes > 0:
+        returnstring += f"{minutes}m "
 
     return returnstring
