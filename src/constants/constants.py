0d84fa8e07b675e66e2743de57d65de908576965
Karlo <e14912km@student.manchester.ac.uk>
2021-05-05 00:48:33 +0200

    Constants s -> ms

--- src/constants/constants.py
+++ src/constants/constants.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
 
-class Timeframe(Enum):
-    S15 = 15
-    M1 = 60
-    M5 = 300
-    M15 = 900
-    H1 = 3600
-    H4 = 14400
-    D1 = 86400
+class Timeframe(Enum):  # in ms
+    S15 = 15 * 1000
+    M1 = 60 * 1000
+    M5 = 5 * 60 * 1000
+    M15 = 15 * 60 * 1000
+    H1 = 60 * 60 * 1000
+    H4 = 4 * 60 * 60 * 1000
+    D1 = 24 * 60 * 60 * 1000
