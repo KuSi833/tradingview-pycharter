10d2c27ad62c72c2fa7012edfb54befb255a398e
Karlo <e14912km@student.manchester.ac.uk>
2021-05-04 16:21:48 +0200

    Improved formatting, support for first parameter

--- src/helpers/formatting.py
+++ src/helpers/formatting.py
@@ -2,15 +2,25 @@ from enum import Enum
 from typing import Union
 
 
-def parameter_formatting(parameter: Union[bool, int, str, Enum], ps_parameter_name: str) -> str:
-    "Given the paremeter and pinescript variable name, returns pinescript string"
+def parameter_formatting(parameter: Union[bool, int, str, Enum], ps_parameter_name: str, add_comma: bool = True) -> str:
+    """
+    Given the paremeter and pinescript variable name, returns pinescript string
+    add_coma = False used when parameter is function
+    """
+    returnstring = ""
+    if add_comma:
+        returnstring += ", "
+    returnstring += f"{ps_parameter_name} = "
+
     if parameter is None:  # None formatting
         return ""
     elif isinstance(parameter, bool):  # Boolean formatting
-        return f", {ps_parameter_name} = {str(parameter).lower()}"
+        returnstring += str(parameter).lower()
     elif isinstance(parameter, str):  # String formatting
-        return f", {ps_parameter_name} = \"{parameter}\""
-    elif isinstance(parameter, int):  # Integer formatting
-        return f", {ps_parameter_name} = {parameter}"
+        returnstring += f"\"{parameter}\""
+    elif isinstance(parameter, int) or isinstance(parameter, float):  # Integer and Float formatting
+        returnstring += str(parameter)
     elif isinstance(parameter, Enum):  # Enum formatting
-        return f", {ps_parameter_name} = {parameter.value}"
+        returnstring += parameter.value
+
+    return returnstring
